cff5845bf11a1436ed604091890913bdd77eb1aa
ErinJordan222 <erin.jordan@du.edu>
2022-05-23 14:44:17 -0600

    added_database_and_install.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // define callback to filter requests
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,43 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Edit to the database');
 
+  const jateDB = await openDB('jate', 1);
+
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ content: content });
+
+  const result = await request;
+
+  console.log('result.value', result);
+  
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
 
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+  
+  const data = await request;
+
+  const result = data.content;
+
+  console.log('result.value', result);
+
+  return result;
+
+  
+  
+}
 initdb();
